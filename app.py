324a1d730067e163f798e0eb6e6494ebb4962eaa
Aayush Sharma <aayush.sharma.k88@gmail.com>
2023-08-01 21:28:58 +0530

    Updated the project

---
+++ app.py
@@ -0,0 +1,82 @@
+# Importing Libraries
+from flask import Flask, render_template, request
+import pickle
+import yfinance as yf
+from datetime import datetime, timedelta
+from sklearn.preprocessing import MinMaxScaler
+import numpy as np
+import pandas as pd
+
+# Initializing the flask app
+app = Flask(__name__,template_folder='templates')
+
+# Opening the pickle files containing the models
+model1 = pickle.load(open('models/model1.pkl','rb'))
+model2 = pickle.load(open('models/model2.pkl','rb'))
+model3 = pickle.load(open('models/model3.pkl','rb'))
+
+# Rendering the index file
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+# Rendering the about page
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+# Predicting Stock Price based on the form input from index.html
+@app.route('/predict', methods=['POST','GET'])
+def predict():
+
+    # Storing the input from the form
+    company = request.form['company']
+
+    # Initializing the start and end dates
+    today = datetime.now()
+    date_60_days_ago = today - timedelta(days=100)
+    end = today.strftime('%Y-%m-%d')
+    start = date_60_days_ago.strftime('%Y-%m-%d')
+
+    # Initializing the MinMax Scaler
+    scaler = MinMaxScaler(feature_range=(0, 1))
+
+    # Downloading the stock price data
+    if company == "google":
+        quote = yf.download('GOOGL', start, end)
+    elif company == "apple":
+        quote = yf.download('AAPL', start, end)
+    elif company == "tesla":
+        quote = yf.download('TSLA', start, end)
+
+    # Preparing the data for prediction
+    df = quote.filter(['Close'])
+    last_60_days = df[-60:].values
+    last_60_days_scaled = scaler.fit_transform(last_60_days)
+    X_test = []
+    X_test.append(last_60_days_scaled)
+    X_test = np.array(X_test)
+    X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1], 1))
+
+    # Predicting the stock price based on the input from the form
+    if company == "google":
+        pred_price = (model1.predict(X_test))
+        pred_price = scaler.inverse_transform(pred_price)
+        return render_template('index.html', pred='Google\'s Stock Price will close today at {:.3f}'.format(pred_price[0][0]))
+    elif company == "apple":
+        pred_price = (model2.predict(X_test))
+        pred_price = scaler.inverse_transform(pred_price)
+        return render_template('index.html', pred='Apple\'s Stock Price will close today at {:.3f}'.format(pred_price[0][0]))
+
+    elif company == "tesla":
+        pred_price = (model3.predict(X_test))
+        pred_price = scaler.inverse_transform(pred_price)
+        return render_template('index.html', pred='Tesla\'s Stock Price will close today at {:.3f}'.format(pred_price[0][0]))
+
+    else:
+        return render_template('index.html', pred='Error')
+
+
+# Running the flask app
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ models/predictionModel.py
@@ -0,0 +1,116 @@
+# Importing Libraries
+import math
+
+import yfinance as yf
+import numpy as np
+import pandas as pd
+import datetime as dt
+
+from sklearn.preprocessing import MinMaxScaler
+from keras import Sequential
+from keras.layers import Dense, LSTM
+
+import pickle
+
+
+# Reading the data
+start = dt.datetime(2012,1,1)
+end = dt.date.today()
+
+company1 = 'AAPL'
+company2 = 'GOOGL'
+company3 = 'TSLA'
+
+df1 = yf.download(company1, start, end)
+df2 = yf.download(company2, start, end)
+df3 = yf.download(company3, start, end)
+
+# Filtering closing stock prices
+data1 = df1.filter(['Close'])
+data2 = df2.filter(['Close'])
+data3 = df3.filter(['Close'])
+
+# Converting the dataframes to numpy arrays
+dataset1 = data1.values
+dataset2 = data2.values
+dataset3 = data3.values
+
+# Getting the length of the datasets
+trainDataLen1 = math.ceil(len(dataset1)*0.8)
+trainDataLen2 = math.ceil(len(dataset2)*0.8)
+trainDataLen3 = math.ceil(len(dataset3)*0.8)
+
+# Scaling the Data
+
+# Initializing the MinMaxScaler
+scaler = MinMaxScaler(feature_range=(0,1))
+
+scaledData1 = scaler.fit_transform(dataset1)
+scaledData2 = scaler.fit_transform(dataset2)
+scaledData3 = scaler.fit_transform(dataset3)
+
+# Creating training dataset
+trainData1 = scaledData1[0:trainDataLen1,:]
+trainData2 = scaledData2[0:trainDataLen2,:]
+trainData3 = scaledData3[0:trainDataLen3,:]
+
+x_train1, y_train1 = [], []
+x_train2, y_train2 = [], []
+x_train3, y_train3 = [], []
+
+for i in range(60, len(trainData1)):
+    x_train1.append(trainData1[i-60:i,0])
+    y_train1.append(trainData1[i,0])
+
+for i in range(60, len(trainData2)):
+    x_train2.append(trainData2[i-60:i,0])
+    y_train2.append(trainData2[i,0])
+
+for i in range(60, len(trainData3)):
+    x_train3.append(trainData3[i-60:i,0])
+    y_train3.append(trainData3[i,0])
+
+x_train1, y_train1 = np.array(x_train1), np.array(y_train1)
+x_train2, y_train2 = np.array(x_train2), np.array(y_train2)
+x_train3, y_train3 = np.array(x_train3), np.array(y_train3)
+
+# Reshaping the training dataset for LSTM model - converting 2-D data to 3-D data
+x_train1 = np.reshape(x_train1, (x_train1.shape[0],x_train1.shape[1],1))
+x_train2 = np.reshape(x_train2, (x_train2.shape[0],x_train2.shape[1],1))
+x_train3 = np.reshape(x_train3, (x_train3.shape[0],x_train3.shape[1],1))
+
+# Building the LSTM model
+model1 = Sequential()
+model1.add(LSTM(50, return_sequences=True, input_shape=(x_train1.shape[1],1)))
+model1.add(LSTM(50, return_sequences=False))
+model1.add(Dense(25))
+model1.add(Dense(1))
+
+model2 = Sequential()
+model2.add(LSTM(50, return_sequences=True, input_shape=(x_train1.shape[1],1)))
+model2.add(LSTM(50, return_sequences=False))
+model2.add(Dense(25))
+model2.add(Dense(1))
+
+model3 = Sequential()
+model3.add(LSTM(50, return_sequences=True, input_shape=(x_train1.shape[1],1)))
+model3.add(LSTM(50, return_sequences=False))
+model3.add(Dense(25))
+model3.add(Dense(1))
+
+# Compiling the model with optimizer and loss function
+model1.compile(optimizer='adam', loss='mean_squared_error')
+model2.compile(optimizer='adam', loss='mean_squared_error')
+model3.compile(optimizer='adam', loss='mean_squared_error')
+
+# Training the model
+model1.fit(x_train1,y_train1, batch_size=1,epochs=1)
+model2.fit(x_train2,y_train2, batch_size=1,epochs=1)
+model3.fit(x_train3,y_train3, batch_size=1,epochs=1)
+
+# Dumping the models to pickle file
+pickle.dump(model1,open('model1.pkl','wb'))
+pickle.dump(model2,open('model2.pkl','wb'))
+pickle.dump(model3,open('model3.pkl','wb'))
+
+print("Done!")
